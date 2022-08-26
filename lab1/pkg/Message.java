d70a6186a9a1ea3727def6568d1868bdb10e6e73
SpeedyRogue1029 <dohjoshua@gmail.com>
2022-08-26 02:31:29 +0000

    updated message class

--- lab1/pkg/Message.java
+++ lab1/pkg/Message.java
@@ -1,17 +1,27 @@
-package pkg;
+ package pkg;
 import java.util.*;
 import java.io.*;
 
 public class Message {
+	private String author;
+	private String subject;
+	private String body;
+	private int MessageNum;
 
 	// Default Constructor
 	public Message() {
-		
+		author = "";
+		subject = "";
+		body = "";
+		MessageNum = 0;
 	}
 	
 	// Parameterized Constructor
 	public Message(String auth, String subj, String bod, int i) {
-		
+		author = auth;
+		subject = subj;
+		body = bod;
+		MessageNum = i;
 	}
 
 	// This function is responsbile for printing the Message
@@ -24,7 +34,16 @@ public class Message {
 	// Note: Each indentation increment represents 2 spaces. e.g. if indentation ==  1, the reply should be indented 2 spaces, 
 	// if it's 2, indent by 4 spaces, etc. 
 	public void print(int indentation){
-
+		for(int cntr = 0; cntr < indentation*2; cntr++)
+		{
+			System.out.print("	");
+		}
+		System.out.println("Message #" + i + ": " + subject);
+		for(int cntr = 0; cntr < indentation; cntr++)
+		{
+			System.out.print("	");
+		}
+		System.out.println("From " + author + ": " + )
 	}
 
 	// Default function for inheritance
@@ -34,12 +53,12 @@ public class Message {
 
 	// Returns the subject String
 	public String getSubject(){
-		
+		return subject;
 	} 
 
 	// Returns the ID
 	public int getId(){
-		
+		return MessageNum;
 	}
 
 	// Adds a child pointer to the parent's childList.
--- lab1/pkg/Reply.java
+++ lab1/pkg/Reply.java
@@ -6,12 +6,12 @@ public class Reply extends Message {
 
 	// Default Constructor
 	public Reply() {
-
+		super;
 	}
 
 	// Parameterized Constructor
 	public Reply(String auth, String subj, String bod, int i) {
-		
+		super;
 	}
 
 	// Returns if this is a reply (true)
